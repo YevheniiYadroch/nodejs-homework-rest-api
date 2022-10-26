486752f7afd18e605fddd3b7d2f08a5b64024723
YevheniiYadroch <evheniiyadroch@gmail.com>
2022-10-26 17:23:51 +0100

    database connection

--- config.js
+++
@@ -1,5 +0,0 @@
-// const DB_HOST = 'mongodb+srv://YevheniiYadroch:psxAzucJs9Ln7XZT@cluster0.vclqc4q.mongodb.net'
-
-// module.exports = {
-//     DB_HOST,
-// }
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
 
-// const app = require('./app')
+const app = require('./app')
 
-dotenv.config()
 
-const { DB_HOST } = process.env
+const { DB_HOST, PORT = 3000 } = process.env
 
 mongoose.connect(DB_HOST)
-  .then(() => console.log('Database connection successful'))
-  .catch(error => console.log(error.message))
+  .then(() => app.listen(PORT))
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000")
